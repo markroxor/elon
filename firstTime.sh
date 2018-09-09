e80aed4528e939ec4fdaf55c25f0e213cff8b08e
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-09-09 11:46:53 +0530

    as service and params

--- firstTime.sh
+++ firstTime.sh
@@ -1,6 +1,29 @@
+#create systemd config
+get_service_conf()
+{
+eval "$1='[Unit]\n
+Description=Logit service\n
+\n
+[Service]\n
+Environment=\"DISPLAY=:0\"\n
+Environment=\"DBUS_SESSION_BUS_ADDRESS=$(echo $DBUS_SESSION_BUS_ADDRESS)\"\n
+User=$(whoami)\n
+ExecStart=/bin/bash /opt/logit.sh\n
+\n
+[Install]\n
+WantedBy=multi-user.target'\n
+"
+}
+
+conf=''
+get_service_conf conf
+sudo echo $conf >/etc/systemd/system/
+
+
 cp ~/logit/logit.sh /opt/
+
 sudo mkdir /var/log/logit
 chmod u+x /opt/logit.sh
-cp logit.service /etc/systemd/system/
+
 sudo systemctl start logit
 sudo systemctl enable logit
--- get_data.py
+++ get_data.py
@@ -1,70 +1,92 @@
 import yaml
 import os
 from os.path import splitext
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-log_dir = "logs/"
+import argparse
 
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+def main(args):
 
-with open("config.yaml", 'r') as config:
-    config = yaml.load(config)
+    if args.days is None:
+        days = 1
+    elif args.days < 1:
+        raise ValueError("--days should be greater than 0.")
+    else:
+        days = args.days
 
-event_types = [*config]
-event_type_time = [0] * len(event_types)
+    print("Plotting for last", days, "day(s).")
 
-log_start_time = datetime(2018, 9, 4, 0, 0).timestamp()
-log_end_time = datetime.now().timestamp()
+    log_dir = "logs/"
 
-span = log_end_time - log_start_time
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-for file_ in os.listdir("logs"):
-    file_name = float(splitext(file_)[0])
-    if file_name >= log_start_time and file_name <= log_end_time:
-        print("Parsing... ", file_)
+    with open("config.yaml", 'r') as config:
+        config = yaml.load(config)
 
-        with open(log_dir + file_, 'rb') as f:
-            for log in f.readlines():
-                log = str(log).split(" ")
+    event_types = [*config]
+    event_type_time = [0] * len(event_types)
 
-                end_time, duration, event = log[0], log[1], " ".join(log[2:])[:-3]
-                event_type = 'other'
+    log_start_time = datetime.today() - timedelta(days=days)
+    log_start_time = log_start_time.timestamp()
 
-                for event_type, event_ in config.items():
-                    for e in event_:
-                        if e in event:
-                            ind_ = event_types.index(event_type)
-                            event_type_time[ind_] += float(duration)/3600.
-                            break
+    log_end_time = datetime.now().timestamp()
 
-print(event_types)
-print(event_type_time)
+    print("From", datetime.fromtimestamp(log_start_time), "to", datetime.fromtimestamp(log_end_time))
 
-fig, ax = plt.subplots(figsize=(10, 5), subplot_kw=dict(aspect="equal"))
+    span = log_end_time - log_start_time
 
-data = event_type_time
-Tasks = event_types
+    for file_ in os.listdir("logs"):
+        file_name = float(splitext(file_)[0])
+        if file_name >= log_start_time and file_name <= log_end_time:
+            print("Parsing... ", file_)
+
+            with open(log_dir + file_, 'rb') as f:
+                for log in f.readlines():
+                    log = str(log).split(" ")
+
+                    end_time, duration, event = log[0], log[1], " ".join(log[2:])[:-3]
+                    event_type = 'other'
+
+                    for event_type, event_ in config.items():
+                        for e in event_:
+                            if e in event:
+                                ind_ = event_types.index(event_type)
+                                event_type_time[ind_] += float(duration)/3600.
+                                break
+
+    fig, ax = plt.subplots(figsize=(10, 5), subplot_kw=dict(aspect="equal"))
+
+    data = event_type_time
+    Tasks = event_types
 
 
-def func(pct, allvals):
-    absolute = pct/100.*np.sum(allvals)
-    return "{:.3f}%\n({:.3f} hrs)".format(pct, absolute)
+    def func(pct, allvals):
+        absolute = pct/100.*np.sum(allvals)
+        return "{:.3f}%\n({:.3f} hrs)".format(pct, absolute)
 
 
-wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
-                                  textprops=dict(color="w"))
+    wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                    textprops=dict(color="w"))
 
-ax.legend(wedges, Tasks,
-          title="Tasks",
-          loc="center left",
-          bbox_to_anchor=(1, 0, 0.5, 1))
+    ax.legend(wedges, Tasks,
+            title="Tasks",
+            loc="center left",
+            bbox_to_anchor=(1, 0, 0.5, 1))
 
-plt.setp(autotexts, size=8, weight="bold")
+    plt.setp(autotexts, size=8, weight="bold")
 
-ax.set_title("Logs for today.")
+    ax.set_title("Logs for today.")
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="An app for logging your daily routines.")
+
+    parser.add_argument('--days', type=int, help='Number of days since today to plot.')
+
+    args = parser.parse_args()
+    main(args)
